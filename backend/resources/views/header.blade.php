33370f8cad05575bdf3964106b69db4a7cc0fa8b
takahirohana <w.hanasta.w@gmail.com>
2020-12-22 10:18:36 +0900

    make it better

--- backend/resources/views/header.blade.php
+++ backend/resources/views/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">ブログ</a>
+  <a class="navbar-brand" href="{{ route('blogs') }}">ブログ</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
